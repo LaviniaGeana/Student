b3b8d55e915dc29f19276d2b59c3c53504d4d027
LaviniaGeana <laviniageorgiana.geana@ulbsibiu.ro>
2026-03-03 16:32:01 +0200

    commit

--- src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -1,4 +1,54 @@
 package ro.ulbs.proiectaresoftware.students;
 
-public class Student {
+public class Student
+{
+    int numarMatricol;
+    String prenume;
+    String nume;
+    String formatieDeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public void setNumarMatricol(int numarMatricol) {
+        this.numarMatricol = numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+
+    public void setFormatieDeStudiu(String formatieDeStudiu) {
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%15d %15s %15s %15s",numarMatricol, prenume, nume, formatieDeStudiu);
+    }
 }
